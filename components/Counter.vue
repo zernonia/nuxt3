d0142e3f5cc0359b3cccd6e8b7dfb6666c28af11
zernonia <59365435+zernonia@users.noreply.github.com>
2021-09-06 12:27:35 +0800

    🌟 Initial commit

---
+++ components/Counter.vue
@@ -0,0 +1,10 @@
+<template>Page visits: {{ data.count }}</template>
+
+<script>
+export default defineNuxtComponent({
+  setup() {
+    const { data } = asyncData("time", () => $fetch("/api/count"))
+    return { data }
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-4xl">
+    <h1>Hello Nuxt3!</h1>
+    <p>How fast it this reloading!</p>
+    <Counter></Counter>
+  </div>
+</template>
+
+<script setup nuxt></script>
